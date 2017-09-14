3997e0871665ce417427fa9b04e303a6e4ef49fb
Idan Entin <idanen@gmail.com>
2017-09-14 09:17:32 +0300

    Simplify tests

--- __test__/checkOverlap.test.js
+++ __test__/checkOverlap.test.js
@@ -35,28 +35,18 @@ describe('check-element-overlap', () => {
   });
 
   describe('swap rects', () => {
-    it('should overlap vertically', () => {
-      rect2.top = 0;
-      rect1.top = 49;
-      expect(checkOverlappingRects(rect1, rect2)).to.equal(true);
+    it('should not find overlap', () => {
+      expect(checkOverlappingRects(rect2, rect1)).to.equal(false);
     });
 
-    it('should find no vertical overlap', () => {
-      rect2.top = 0;
-      rect1.top = 50;
-      expect(checkOverlappingRects(rect1, rect2)).to.equal(false);
+    it('should overlap vertically', () => {
+      rect2.top = rect1.height - 1;
+      expect(checkOverlappingRects(rect2, rect1)).to.equal(true);
     });
 
     it('should overlap horizontally', () => {
-      rect2.left = 0;
-      rect1.left = 99;
-      expect(checkOverlappingRects(rect1, rect2)).to.equal(true);
-    });
-
-    it('should find no horizontal overlap', () => {
-      rect2.left = 0;
-      rect1.left = 100;
-      expect(checkOverlappingRects(rect1, rect2)).to.equal(false);
+      rect2.left = rect1.width - 1;
+      expect(checkOverlappingRects(rect2, rect1)).to.equal(true);
     });
   });
 });
